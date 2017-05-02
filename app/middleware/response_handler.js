19c9a772caf17e3d45df0c6144e3954ea3bdfa4b
lovestar1018 <gang.liu@ymm56.com>
2017-05-02 15:38:16 +0800

    初始化修改

---
+++ app/middleware/response_handler.js
@@ -0,0 +1,25 @@
+/**
+ * Created by star on 2017/5/2.
+ */
+'use strict';
+module.exports = () => {
+  return function* (next) {
+    try {
+      yield next;
+      const result = this.body;
+      // 包装返回参数
+      this.body = {
+        code: 1,
+        msg: 'success',
+        data: result,
+      };
+    } catch (err) {
+      // 所有的异常都在 app 上触发一个 error 事件，框架会记录一条错误日志
+      this.app.emit('error', err, this);
+      this.body = {
+        code: err.status,
+        msg: this.app.config.env === 'prod' ? 'Internal Server Error' : err.message,
+      };
+    }
+  };
+};
