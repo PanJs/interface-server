1de8a3f7f173faacbc82b718dff7e35e5873db45
kevan <258137678@qq.com>
2017-05-02 18:52:06 +0800

    加解密模块 注释

--- app/extend/des.js
+++ app/extend/des.js
@@ -4,17 +4,29 @@
 'use strict';
 const crypto = require('crypto');
 module.exports = {
-  encrypt(plaintext, key) {
+  /**
+   * 加密
+   * @param {string} str 加密字符串
+   * @param {string} key 加密秘钥
+   * @return {*} 加密后的字符串
+   */
+  encrypt(str, key) {
     const cipher = crypto.createCipheriv('des-ecb', key, '');
-    cipher.setAutoPadding(true)
-    let ciph = cipher.update(plaintext, 'utf8', 'base64');
+    cipher.setAutoPadding(true);
+    let ciph = cipher.update(str, 'utf8', 'base64');
     ciph += cipher.final('base64');
     return ciph;
   },
-  decrypt(encrypt_text, key) {
+  /**
+   * dec解密
+   * @param {string} str 加密字符串
+   * @param {string} key 加密秘钥
+   * @return {*} 解密后的字符串
+   */
+  decrypt(str, key) {
     const decipher = crypto.createDecipheriv('des-ecb', key, '');
     decipher.setAutoPadding(true);
-    let txt = decipher.update(encrypt_text, 'base64', 'utf8');
+    let txt = decipher.update(str, 'base64', 'utf8');
     txt += decipher.final('utf8');
     return txt;
   },
