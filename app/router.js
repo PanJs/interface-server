eb10d16e8f1b28916a0873f7034e723ac5cf6134
lovestar1018 <gang.liu@ymm56.com>
2017-05-03 14:00:04 +0800

    增加登录、token处理

--- app/router.js
+++ app/router.js
@@ -1,8 +1,7 @@
 'use strict';
 
 module.exports = app => {
-  app.get(`${app.config.pkg.path}/kaptcha/init`, 'kaptcha.init');
-  app.get(`${app.config.pkg.path}/test`, 'home.index');
   const loginHandler = app.middlewares.loginHandler;
   app.get(`${app.config.appConfig.path}/test`, loginHandler, 'home.index');
+  app.get(`${app.config.pkg.path}/kaptcha/init`, 'kaptcha.init');
 };
