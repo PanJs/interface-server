9677da5807ddf7fcf5dc51ea72e96e9c4e3fd746
kevan <258137678@qq.com>
2017-05-03 10:45:23 +0800

    验证码模块

--- app/service/kaptcha.js
+++ app/service/kaptcha.js
@@ -13,10 +13,10 @@ const captchapng = require('captchapng');
 module.exports = app => {
   class User extends app.Service {
     * getCaptcha(key) {
-      var p = new captchapng(80,30,key); // width,height,numeric captcha
+      const p = new captchapng(80, 30, key); // width,height,numeric captcha
       p.color(0, 0, 0, 0);  // First color: background (red, green, blue, alpha)
       p.color(80, 80, 80, 255); // Second color: paint (red, green, blue, alpha)
-      var img = p.getBase64();
+      const img = p.getBase64();
       return img
     }
   }
